11d1f9d530475cc8ec21f0faafe2ccae2d7f5480
Filip Povolny <filip.povolny@gmail.com>
2020-07-24 08:48:26 +0200

    Remove debug prints in generator_model.py.

--- familyGan/stylegan_encoder/encoder/generator_model.py
+++ familyGan/stylegan_encoder/encoder/generator_model.py
@@ -54,9 +54,6 @@ class Generator:
         self.sess = tf.get_default_session()
         self.graph = tf.get_default_graph()
 
-        for n in self.graph.as_graph_def().node:
-            print(n.name)
-
         self.dlatent_variable = next(v for v in tf.global_variables() if 'learnable_dlatents' in v.name)
         self.set_dlatents(self.initial_dlatents)
 
